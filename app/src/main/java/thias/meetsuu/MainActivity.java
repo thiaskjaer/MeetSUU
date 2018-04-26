1bbfc132e65abf48cc0199ec6c918d57926945c1
Thias Kjaer <s.thias@gmail.com>
2018-04-25 22:11:09 -0600

    Added method to add activity from FAB

--- app/src/main/java/thias/meetsuu/MainActivity.java
+++ app/src/main/java/thias/meetsuu/MainActivity.java
@@ -1,16 +1,19 @@
 
 package thias.meetsuu;
 
+        import android.support.design.widget.FloatingActionButton;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.support.v7.widget.LinearLayoutManager;
         import android.support.v7.widget.RecyclerView;
+        import android.view.View;
 
         import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Activity> activityList = new ArrayList<>();
     private RecyclerView rv;
+    private FloatingActionButton fb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recycle);
 
         rv=findViewById(R.id.rv);
+        fb=findViewById(R.id.newPost);
 
         LinearLayoutManager llm = new LinearLayoutManager(this);
         rv.setLayoutManager(llm);
         rv.setHasFixedSize(true);
+        fb.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                addCard();
+                initializeAdapter();
+            }
+        });
 
         initializeData();
         initializeAdapter();
@@ -30,9 +40,13 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initializeData(){
-        activityList = new ArrayList<>();
-        activityList.add(new Activity("Anyone wanna play soccer?","If anyone is down for some soccer, we can play on school's field. I'm thinking 4v4 or 2v2", 2));
-        activityList.add(new Activity("Settles of Catan","Let's play some Settlers!", 2));
+        //activityList = new ArrayList<>();
+        activityList.add(new Activity("Anyone wanna play soccer?","If anyone is down for some soccer, we can play on the school's field. I'm thinking 4v4 or 2v2", 2));
+        activityList.add(new Activity("Settlers of Catan","Let's play some Settlers!", 2));
+    }
+
+    private void addCard(){
+        activityList.add(new Activity("Settlers of Catan","Let's play some Settlers!", 2));
     }
 
     private void initializeAdapter(){
