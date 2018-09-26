2e1101a41c93c94714b5df9261aefcdfc1ada0c4
Thias Kjaer <s.thias@gmail.com>
2018-09-25 20:44:14 -0600

    Initialized buttons for EditPostActivity and updated Android Studio

--- app/src/main/java/thias/meetsuu/EditPostActivity.java
+++ app/src/main/java/thias/meetsuu/EditPostActivity.java
@@ -2,12 +2,24 @@ package thias.meetsuu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class EditPostActivity extends AppCompatActivity {
+    private TextView titleField, descField;
+    private Spinner themeField;
+    private Button postButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_post);
+        titleField = findViewById(R.id.titleField);
+        descField = findViewById(R.id.descField);
+        themeField = findViewById(R.id.themeField);
+        postButton = findViewById(R.id.postButton);
     }
+
 }
